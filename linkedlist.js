8e2db1d6d41554b3002f6375a9cc64c5beb7226e
Fabius Gasber <fabiusgasber@gmail.com>
2025-01-13 13:57:10 +0100

    feat: add function to get last element (tail) of list

--- linkedlist.js
+++ linkedlist.js
@@ -2,3 +2,10 @@
 
     const getHead = () => head;
 
+    const getTail = () => {
+        if(size() === 0) return null;
+        let temp = getHead();
+        while(temp.getNext() !== null) temp = temp.getNext();
+        return temp;
+    }
+
